e361b66072c866b1f856853c3ddddb4a3e1da7dd
amit balyan <amitkr.balyan@gmail.com>
2020-05-30 16:41:34 +0530

    commit with update the code

---
+++ FlipcartDemoProject/src/com/flipcad/bean/Employee.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.flipcad.bean;
+
+/**
+ * @author Amit
+ *
+ */
+public class Employee {
+	
+	private int empId;
+	private String empName;
+	
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	
+	
+	
+
+}
--- FlipcartDemoProject/src/com/flipcard/demo/DemoProject.java
+++ FlipcartDemoProject/src/com/flipcard/demo/DemoProject.java
@@ -1,10 +1,18 @@
 package com.flipcard.demo;
 
+import com.flipcad.bean.Employee;
+
 public class DemoProject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      System.out.println("demo started");
+     
+     Employee emp=new Employee();
+     emp.setEmpId(101);
+     emp.setEmpName("Employee Training");
+     System.out.println(emp.getEmpId());
+     System.out.println(emp.getEmpName());
 	}
 
 }
