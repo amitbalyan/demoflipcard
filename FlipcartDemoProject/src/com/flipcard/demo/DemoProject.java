a6a854060225654a4e67ef27e8366d4b5eee8b11
amit balyan <amitkr.balyan@gmail.com>
2020-05-30 16:31:01 +0530

    commit with push the structure of project

---
+++ FlipcartDemoProject/src/com/flipcard/demo/DemoProject.java
@@ -0,0 +1,10 @@
+package com.flipcard.demo;
+
+public class DemoProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("demo started");
+	}
+
+}
